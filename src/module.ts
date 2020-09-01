5b0a8986f41e92da0ccc14c0c68d2d24ee840ac0
Nam Nguyen <nam.nguyen@inlumina.work>
2020-09-01 21:55:28 +0700

    Create grafana-alerta UI

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,9 @@
 import { PanelPlugin } from '@grafana/data';
 
 import { GrafanaAlertaOptions } from './types';
-import { GrafanaAlertaPanel } from './GrafanaAlertaPanel';
+import { AppComponent } from './App';
 import { optionsBuilder } from './options';
 
-export const plugin = new PanelPlugin<GrafanaAlertaOptions>(GrafanaAlertaPanel)
+export const plugin = new PanelPlugin<GrafanaAlertaOptions>(AppComponent)
   .setNoPadding()
   .setPanelOptions(optionsBuilder);
